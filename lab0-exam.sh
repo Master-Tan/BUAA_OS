68221f77d770e9f930faa8827c337d4824d6c6ec
GitLab <gitlab@git.os.buaa.edu.cn>
2022-03-17 15:19:08 +0800

    Judgement for lab0-Extra at 2022-03-17T15:19:08+08:00

--- lab0-exam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-make
-touch hello_os
-mkdir hello_os_dir
-cp os_hello ./hello_os_dir/
-cp os_hello ./hello_os_dir/hello_os
-rm os_hello
-touch hello_os.txt
-grep -i -n "os_hello" hello_os.c > hello_os.txt
--- test.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-	int a,b;
-	if(2<3<4)
-	{
-		scanf("%d",&a);
-		printf("%d\n",a+a);
-	}
-}
