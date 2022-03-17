ab4b055395362dd486ac8d44872bb7bb6e8567b0
20373864 <tanlide@126.com>
2022-03-17 15:47:21 +0800

    20373864

--- find_warnings.sh
+++ find_warnings.sh
@@ -2,7 +2,7 @@
 gcc -Wall ${1} -o test 2> warning.txt
 b=$?
 grep "warning" warning.txt > tanlide.txt
-sed 's/warning: //g' tanlide.txt >result.txt
+sed 's/warning: //g' tanlide.txt > result.txt
 rm tanlide.txt
 a=1
 if [ $b -eq 0 ]
