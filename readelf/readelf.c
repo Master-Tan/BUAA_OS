400d02eeeabb822101ec97ae1f84db12b4fef891
20373864 <tanlide@126.com>
2022-03-24 15:32:44 +0800

    20373864

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -75,8 +75,8 @@ int readelf(u_char *binary, int size)
 		Elf32_Off first_page;
 		for (Nr = 0; Nr < ph_entry_count; Nr++){
 			if (Nr != ph_entry_count - 1) {
-				Elf32_Addr addr = (phdr + Nr)->p_vaddr + (Elf32_Addr)((phdr + Nr)->p_memsz);
-				Elf32_Addr next_addr = (phdr + (Nr + 1))->p_vaddr;
+				Elf32_Addr addr = (phdr + Nr)->p_vaddr + (Elf32_Addr)((phdr + Nr)->p_memsz) - (Elf32_Addr)1;
+				Elf32_Addr next_addr = (phdr + (Nr + 1))->p_vaddr - (Elf32_Addr)1;
 //				if ((next_addr) != (Elf32_Addr)0){
 					if (addr >= next_addr){
 						flag = 1;
