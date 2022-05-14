ceb7d850b96722c263660370ec1d5f2981028f99
20373864 <tanlide@126.com>
2022-05-15 03:54:06 +0800

    2022/5/15/3:49

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -142,7 +142,7 @@ int sys_set_pgfault_handler(int sysno, u_int envid, u_int func, u_int xstacktop)
 /*** exercise 4.3 ***/
 int sys_mem_alloc(int sysno, u_int envid, u_int va, u_int perm)
 {
-/*
+
 	// Your code here.
 	struct Env *env;
 	struct Page *ppage;
@@ -168,7 +168,7 @@ int sys_mem_alloc(int sysno, u_int envid, u_int va, u_int perm)
 		return ret;
 	}
 	return 0;
-*/
+
 }
 
 /* Overview:
