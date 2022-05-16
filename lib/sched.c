89af5fcd16d142842a1d103e3222ee732284b7dd
20373864 <tanlide@126.com>
2022-05-16 11:58:39 +0800

    2

--- lib/sched.c
+++ lib/sched.c
@@ -16,7 +16,38 @@ void sched_yield(void)
 {
     static int count = 0; // remaining time slices of current env
     static int point = 0; // current env_sched_list index
-    
+	if (count == 0 || e == NULL || e->env_status != ENV_RUNNABLE)
+	{
+		if (e != NULL)
+		{
+			LIST_REMOVE(e, env_sched_link);
+			if (e->env_status != ENV_FREE)
+			{
+				LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
+			}
+		}
+		while (1)
+		{
+			while (LIST_EMPTY(&env_sched_list[point]))
+				point = 1 - point;
+			e = LIST_FIRST(&env_sched_list[point]);
+			if (e->env_status == ENV_FREE)
+				LIST_REMOVE(e, env_sched_link);
+			else if (e->env_status == ENV_NOT_RUNNABLE)
+			{
+				LIST_REMOVE(e, env_sched_link);
+				LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
+			}
+			else
+			{
+				count = e->env_pri;
+				break;
+			}
+		}
+	}
+	count--;
+	e->env_runs++;
+	env_run(e);    
     /*  hint:
      *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
      *     using LIST_REMOVE and LIST_INSERT_TAIL.
