4dc0a15a8e03e9d80a4355b4cf8e4baae9dbd1f3
20373864 <tanlide@126.com>
2022-05-26 16:05:00 +0800

    finish exam

--- user/shmtest.c
+++ user/shmtest.c
@@ -3,19 +3,16 @@
 
 void umain() {
     volatile u_int *a = (volatile u_int *) 0x23333334;
-    writef(make_shared((void *) a));
-    *a = 233;
-	writef("Shared!\n");
+    make_shared((void *) a);
+	*a = 233;
     if (fork() == 0) {
         u_int ch = syscall_getenvid();
         *a = ch;
         while (*a == ch)
-			//writef("parent is %x\n", *a);
             syscall_yield();
         writef("parent is %x\n", *a);
     } else {
         while (*a == 233)
-			//writef("child is %x\n", *a);
             syscall_yield();
         writef("child is %x\n", *a);
         *a = syscall_getenvid();
