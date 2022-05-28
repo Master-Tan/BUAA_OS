ac3b5c2d5e092677976543e044160e8ba6405b17
20373864 <tanlide@126.com>
2022-05-29 05:49:10 +0800

    2

--- fs/fs.c
+++ fs/fs.c
@@ -199,8 +199,14 @@ free_block(u_int blockno)
 {
 	// Step 1: Check if the parameter `blockno` is valid (`blockno` can't be zero).
 
+	if (blockno == 0 || (super != NULL && blockno >= super->s_nblocks)) {
+		return;
+	}
+
 	// Step 2: Update the flag bit in bitmap.
 	// you can use bit operation to update flags, such as  a |= (1 << n) .
+
+	bitmap[blockno / 32] |= 1 << (blockno % 32);
 
 }
 
