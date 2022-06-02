826a23886edb358ce7066358c33870a77587b15a
GitLab <gitlab@git.os.buaa.edu.cn>
2022-06-02 13:34:13 +0800

    Initial lab6

--- init/init.c
+++ init/init.c
@@ -14,8 +14,12 @@ void mips_init() {
 
 	env_init();
 
-	// ENV_CREATE(user_devtst);
-	ENV_CREATE(user_fstest);
+	//ENV_CREATE(user_fktest);
+	//ENV_CREATE(user_pingpong);
+	ENV_CREATE(user_testpipe);
+	//ENV_CREATE(user_testpiperace);
+	//ENV_CREATE(user_testptelibrary);
+	//ENV_CREATE(user_icode);
 	ENV_CREATE(fs_serv);
  
 	trap_init();
