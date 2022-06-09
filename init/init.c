7b03ae239a75c9c611997e4adfdd709cb153bbe6
20373864 <tanlide@126.com>
2022-06-09 15:28:30 +0800

    2

--- init/init.c
+++ init/init.c
@@ -15,9 +15,9 @@ void mips_init() {
 	env_init();
 
 	// ENV_CREATE(user_devtst);
-	// ENV_CREATE(user_fstest);
-	// ENV_CREATE(fs_serv);
-	ENV_CREATE(fs_extra);
+	ENV_CREATE(user_fstest);
+	ENV_CREATE(fs_serv);
+	// ENV_CREATE(fs_extra);
 
 	trap_init();
 	kclock_init();
