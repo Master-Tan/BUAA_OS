7cfc0499028d8566160aea1c2946bb2d325c840d
20373864 <tanlide@126.com>
2022-06-12 03:16:29 +0800

    2

--- user/sh.c
+++ user/sh.c
@@ -83,6 +83,8 @@ runcmd(char *s)
 	char *argv[MAXARGS], *t;
 	int argc, c, i, r, p[2], fd, rightpipe;
 	int fdnum;
+	int pid;
+    struct Stat state;
 	rightpipe = 0;
 	gettoken(s, 0);
 again:
@@ -106,12 +108,35 @@ again:
 			}
 			// Your code here -- open t for reading,
 			// dup it onto fd 0, and then close the fd you got.
-			user_panic("< redirection not implemented");
+			r = stat(t, &state);
+                        if (r < 0)
+                        {
+                                writef("cannot open file\n");
+                                exit();
+                        }
+                        fdnum = open(t, O_RDONLY);
+                        dup(fdnum, 0);
+                        close(fdnum);
+
+			//user_panic("< redirection not implemented");
 			break;
 		case '>':
+			if (gettoken(0, &t) != 'w')
+                        {
+                                writef("syntax error: > not followed by word\n");
+                                exit();
+                        }
+
+
 			// Your code here -- open t for writing,
 			// dup it onto fd 1, and then close the fd you got.
-			user_panic("> redirection not implemented");
+			r = stat(t, &state);
+                        fdnum = open(t, O_WRONLY | O_CREAT);
+                        dup(fdnum, 1);
+                        close(fdnum);
+
+
+			//user_panic("> redirection not implemented");
 			break;
 		case '|':
 			// Your code here.
